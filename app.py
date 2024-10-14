d83c6c2a2eb32d4cd83aa2da3654f2e5c2929bc6
Ameur56 <boughershaouari@gmail.com>
2024-10-14 23:56:12 +0100

    Organized Authentification Files

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 @app.route('/tracks/<int:track_id>')
 def get_track(track_id):
     return render_template(f'track_{track_id}.html')
